4ab5da967683307552061ac2faeae5cabf428d28
Rob Nielsen <rob@refactor.com.au>
2021-08-24 02:32:53 +0000

    Initial commit

---
+++ src/main/groovy/com/example/Book.groovy
@@ -0,0 +1,14 @@
+package com.example
+import io.micronaut.core.annotation.NonNull
+import io.micronaut.core.annotation.Introspected
+import javax.validation.constraints.NotBlank
+import groovy.transform.CompileStatic
+
+@CompileStatic
+@Introspected
+class Book {
+
+    @NonNull
+    @NotBlank
+    String name
+}
